0c1040ba6dc32291612fe0935599c7e6c3ecedc2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-23 20:36:52 -0300

    teste

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_route" "public_route" {
   resource_group_name   = azurerm_resource_group.rg.name
   route_table_name      = azurerm_route_table.public_rt.name
   address_prefix        = "0.0.0.0/0"
-  next_hop_internet = "Internet"
+  next_hop_type         = "Internet"
 }
 
 resource "azurerm_subnet_route_table_association" "subnet_public_association" {
